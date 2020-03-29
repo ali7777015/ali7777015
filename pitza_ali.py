e2a8212b0e81bfeedd6e6d7fc47b31e89996a2e2
Your Name <you@example.com>
2020-03-28 19:36:46 -0700

    add pitza_ali.py

---
+++ pitza_ali.py
@@ -0,0 +1,11 @@
+
+
+
+shoa = input ("please give me the shoa:")
+shoa_number = int (shoa)        
+shoa_tavan_do =shoa_number * shoa_number
+ip = 3.14
+
+pitza_ali = ip * shoa_tavan_do
+
+print (pitza_ali)
