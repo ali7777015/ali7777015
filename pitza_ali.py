d672b69be62550525c63256f4fd5f3acb62a23c8
Your Name <you@example.com>
2020-03-28 19:43:26 -0700

    add 4.14 tag

--- pitza_ali.py
+++ pitza_ali.py
@@ -1,10 +1,8 @@
 
-
-
 shoa = input ("please give me the shoa:")
 shoa_number = int (shoa)        
 shoa_tavan_do =shoa_number * shoa_number
-ip = 3.14
+ip = 4.14
 
 pitza_ali = ip * shoa_tavan_do
 
